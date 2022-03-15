88506eead243dbb6fdad66ea895223aa9c1bbd9e
dydtmd6801 <dydtmd6801@naver.com>
2022-03-15 21:38:27 +0900

    db connect select practice

--- java-db-connect/src/SQLSelect.java
+++ java-db-connect/src/SQLSelect.java
@@ -1,9 +1,36 @@
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
+import javax.xml.crypto.Data;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class SQLSelect {
+    public static class DB_data {
+        String a;
+        int b;
+
+        public DB_data(){}
+
+        public void setA(String a){
+            this.a = a;
+        }
+
+        public void setB(int b){
+            this.b = b;
+        }
+
+        public String getA(){
+            return a;
+        }
+
+        public int getB(){
+            return b;
+        }
+    }
+
     public static void main(String[] args) {
         Connection conn = null;
 
@@ -20,7 +47,26 @@ public class SQLSelect {
 
         try{
             conn = (Connection) DriverManager.getConnection(server, userId, passwd);
-            System.out.println("연결성공");
+
+            String sql = "select * from test";
+
+            Statement stmt = (Statement) conn.createStatement();
+            ResultSet rs = (ResultSet) stmt.executeQuery(sql);
+
+            ArrayList<DB_data> data = new ArrayList<DB_data>();
+
+            while(rs.next()){
+                DB_data data_detail = new DB_data();
+                data_detail.setA(rs.getString("a"));
+                data_detail.setB(rs.getInt("b"));
+                data.add(data_detail);
+            }
+
+            for(int i = 0; i < data.size(); i++){
+                System.out.println(data.get(i).getA());
+                System.out.println(data.get(i).getB());
+            }
+
         } catch (SQLException e) {
             System.out.println("연결실패");
             e.printStackTrace();
