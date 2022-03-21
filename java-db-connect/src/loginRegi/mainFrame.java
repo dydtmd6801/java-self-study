f2537206490fd3e7d6c670981082eab80402ed8e
dydtmd6801 <dydtmd6801@naver.com>
2022-03-21 14:04:53 +0900

    register function fix

--- java-db-connect/src/loginRegi/mainFrame.java
+++ java-db-connect/src/loginRegi/mainFrame.java
@@ -1,6 +1,7 @@
 package loginRegi;
 
 import javax.swing.*;
+import javax.swing.plaf.nimbus.State;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,6 +9,8 @@ import java.sql.*;
 
 public class mainFrame extends JFrame implements ActionListener {
     Connection conn = null;
+
+    boolean checkDupl = false;
 
     Login login = new Login();
     Register regi = new Register();
@@ -42,8 +45,9 @@ public class mainFrame extends JFrame implements ActionListener {
     private void eventHandler() {
         login.regiBtn.addActionListener(this);
         login.loginBtn.addActionListener(this);
-
         regi.cancel.addActionListener(this);
+        regi.check.addActionListener(this);
+        regi.register.addActionListener(this);
     }
 
     public static void main(String[] args) {
@@ -71,19 +75,93 @@ public class mainFrame extends JFrame implements ActionListener {
             login.loginPage.setVisible(true);
             regi.regiPage.setVisible(false);
         }
+        if(e.getSource() == regi.check){
+            String regiIdData = regi.idTextField.getText();
+            String duplSQL = "select id from userInfo where id='" + regiIdData + "'";
+
+            duplFunc(duplSQL);
+        }
+        if(e.getSource() == regi.register){
+            String username = regi.idTextField.getText();
+
+            StringBuffer pwData = new StringBuffer();
+            char[] pwCharData = regi.pwTextField.getPassword();
+            for(int i = 0; i < pwCharData.length; i++){
+                pwData.append(pwCharData[i]);
+            }
+
+            StringBuffer confirmPw = new StringBuffer();
+            char[] confirmPwCharData = regi.cfTextField.getPassword();
+            for(int i = 0; i < confirmPwCharData.length; i++){
+                confirmPw.append(confirmPwCharData[i]);
+            }
+
+            String pwDataString = pwData.toString();
+            String confirmPwDataString = confirmPw.toString();
+
+            if(pwDataString.equals(confirmPwDataString)) {
+                if(checkDupl == true) {
+                    String sql = "insert into userInfo values ('" + username + "','" + pwData + "')";
+                    regiFunc(sql);
+
+                    JOptionPane.showMessageDialog(null, "회원가입이 완료되었습니다");
+
+                    regi.regiPage.setVisible(false);
+                    login.loginPage.setVisible(true);
+                } else {
+                    JOptionPane.showMessageDialog(null,"중복확인을 해주세요", "", JOptionPane.PLAIN_MESSAGE);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null,"비밀번호가 맞지 않습니다","",JOptionPane.PLAIN_MESSAGE);
+            }
+        }
+    }
+
+    public Statement dbConnection() throws SQLException {
+        conn = DriverManager.getConnection(server, username, password);
+
+        Statement stmt = conn.createStatement();
+
+        return stmt;
     }
 
     public void loginFunc(String sql) {
         try{
-            conn = DriverManager.getConnection(server, username, password);
+            Statement stmt = dbConnection();
 
-            Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
 
             if(rs.next()){
-                JOptionPane.showMessageDialog(null, "로그인 되었습니다.", "로그인 상태", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null, "로그인 되었습니다", "", JOptionPane.PLAIN_MESSAGE);
             } else {
-                JOptionPane.showMessageDialog(null, "로그인 정보가 맞지않습니다.", "로그인 상태", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null, "로그인 정보가 맞지않습니다", "", JOptionPane.PLAIN_MESSAGE);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void regiFunc(String sql) {
+        try {
+            Statement stmt = dbConnection();
+            stmt.execute(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void duplFunc(String sql) {
+        try {
+            Statement stmt = dbConnection();
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            if(rs.next()){
+                checkDupl = false;
+                JOptionPane.showMessageDialog(null, "중복된 아이디 입니다.", "중복확인", JOptionPane.PLAIN_MESSAGE);
+            } else {
+                checkDupl = true;
+                JOptionPane.showMessageDialog(null, "사용 가능 아이디 입니다.", "중복 확인", JOptionPane.PLAIN_MESSAGE);
             }
         } catch (SQLException e) {
             e.printStackTrace();
